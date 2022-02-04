03c192def971e076cb977709eceb935223edb1ca
Mick Campbell <mick@toolah.com>
2022-02-04 21:49:21 +1000

    Better update like sometime this century

---
+++ docker/learn-terraform-aws-instance/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  cloud {
+    organization = "<Threat Hunt>"
+    workspaces {
+      name = "Workspace1"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-southeast-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0d2f34c92aa48cd95"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
